72fb28c0ed34be19bda277165a90230809c0cdc7
DBNBESPALOV <bdn.bespalov@gmail.com>
2019-12-17 20:03:06 +0300

    add Icons

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,34 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 
 public class Main extends Application {
-//    @Override
-//    public void start(Stage primaryStage) throws Exception {
-//       // DirectoryChoosers directoryChoosers = new DirectoryChoosers();
-//        OpenFileChooser openFileChooser = new OpenFileChooser();
-//        openFileChooser.start(primaryStage);
-//    }
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-      //  Parent root = FXMLLoader.load(Main.class.getResource("/UI/gridPaneXml.fxml"));
         Parent root = FXMLLoader.load(Main.class.getResource("/UI/TabPanelGZ.fxml"));
-
         primaryStage.setTitle("Помогатор для 'АЦК-ГосЗаказ'");
-        //primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/fixx.png")));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
     }
     public static void main(String[] args) {
         launch(args);
     }
-
-
 }
